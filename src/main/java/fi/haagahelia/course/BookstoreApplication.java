d617c504cc934aa5b4ebc96d308c020c889fdd95
Hasegawa Kayo <a1600597@myy.haaga-helia.fi>
2017-02-09 11:36:49 +0200

    Adding new method...

--- src/main/java/fi/haagahelia/course/BookstoreApplication.java
+++ src/main/java/fi/haagahelia/course/BookstoreApplication.java
@@ -1,12 +1,34 @@
 package fi.haagahelia.course;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import fi.haagahelia.course.domain.Book;
+import fi.haagahelia.course.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository repository){
+		return (args) -> {
+			log.info("save a couple of books");
+			repository.save(new Book(1, "AAA", "aaa", 1990, "a123", 10));
+			repository.save(new Book(4, "BBB", "bbb", 2009, "c900", 15));
+			
+			for (Book book : repository.findAll()){
+				log.info(book.toString());
+			}
+		};
 	}
 }
--- src/main/java/fi/haagahelia/course/domain/Book.java
+++ src/main/java/fi/haagahelia/course/domain/Book.java
@@ -1,19 +1,40 @@
 package fi.haagahelia.course.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
 	private String title;
 	private String author;
 	private int year;
 	private String isbn;
 	private int price;
 	
-	public Book(String title, String author, int year, String isbn, int price) {
+	protected Book(){}
+	
+	public Book(long id, String title, String author, int year, String isbn,
+			int price) {
 		super();
+		this.id = id;
 		this.title = title;
 		this.author = author;
 		this.year = year;
 		this.isbn = isbn;
 		this.price = price;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
 	}
 
 	public String getTitle() {
@@ -58,10 +79,10 @@ public class Book {
 
 	@Override
 	public String toString() {
-		return "Book [title=" + title + ", author=" + author + ", year=" + year
-				+ ", isbn=" + isbn + ", price=" + price + "]";
+		return "Book [id=" + id + ", title=" + title + ", author=" + author
+				+ ", year=" + year + ", isbn=" + isbn + ", price=" + price
+				+ "]";
 	}
-	
 	
 
 }
